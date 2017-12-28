5771466a65c6c8a747ff038da92bb1386c9498e3
WraRapap <152708915@qq.com>
2017-12-28 13:48:48 +0800

    delelte
    test file

--- website/controllers/aa.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    var app = angular.module('PH');
-    app.controller('myCtrl', function($scope) {
-        alert("11");
-        $scope.firstName = "John";
-        $scope.lastName = "Doe";
-    });
-})
